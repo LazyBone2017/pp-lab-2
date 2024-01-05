fe1fe31562ef8dc5a4a74f65a072b1c4bbaf6dc1
Jonas Schröter <jonas.schroeter@stud.tu-darmstadt.de>
2024-01-05 22:19:42 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonas/Dokumente/Semester VII/SPP/lab2/cmake-build-debug/_deps/googletest-src
-# Build directory: /home/jonas/Dokumente/Semester VII/SPP/lab2/cmake-build-debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/test_get_path.cpp
+++
@@ -1,2 +0,0 @@
-#include <filesystem>
-std::filesystem::path get_path() { return "/home/jonas/Dokumente/Semester VII/SPP/lab2"; }
